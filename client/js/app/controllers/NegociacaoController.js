7ac43ae7cb2d9d69b9a66a278faa7168d3a765ff
vinicius <viniciusrosa444@gmail.com>
2020-03-17 04:39:40 -0400

    iniciando 3 topico modulo 2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,45 +5,35 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        // this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
         // this._listaNegociacoes = new ListaNegociacoes((model) => this._negociacoesView.update(model));
-        let self =this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
-            get(target,prop,receiver) {
-                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop] == typeof(Function))){
-                    return function () {
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target,prop,receiver)
-            }
-        });
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(),new NegociacoesView($('#negociacoesView')),
+                'adiciona','esvazia'
+            );
+        // this._negociacoesView.update(this._listaNegociacoes);
+        // this._mensagemView = new MensagemView($('#mensagemView'));
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Bind(
+            new Mensagem,
+            new MensagemView($('#mensagemView')),
+            'texto');
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+            // ProxyFactory.create(new Mensagem(),
+            // ['texto'],model => this._mensagemView.update(model));
+        // this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = "Adicionada com Sucesso";
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();
     }
 
     apaga(){
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = "Negociações apagadas com sucesso";
-
-        this._mensagemView.update(this._mensagem)
     }
 
     _criaNegociacao(){
